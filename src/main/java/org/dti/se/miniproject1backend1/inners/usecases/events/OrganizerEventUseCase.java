b3150af54c31e173144b303ef9b9bf8fe20fc9f4
nurfad7 <nurfad7@gmail.com>
2024-12-03 20:22:54 +0700

    add function to create new event

--- src/main/java/org/dti/se/miniproject1backend1/inners/usecases/events/OrganizerEventUseCase.java
+++ src/main/java/org/dti/se/miniproject1backend1/inners/usecases/events/OrganizerEventUseCase.java
@@ -1,5 +1,6 @@
 package org.dti.se.miniproject1backend1.inners.usecases.events;
 
+import org.dti.se.miniproject1backend1.inners.models.entities.*;
 import org.dti.se.miniproject1backend1.inners.models.valueobjects.Session;
 import org.dti.se.miniproject1backend1.inners.models.valueobjects.events.CreateEventRequest;
 import org.dti.se.miniproject1backend1.inners.models.valueobjects.events.RetrieveEventResponse;
@@ -11,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,6 +32,18 @@ public class OrganizerEventUseCase {
 
     @Autowired
     BasicEventUseCase basicEventUseCase;
+
+    @Autowired
+    EventTicketRepository eventTicketRepository;
+
+    @Autowired
+    private EventTicketFieldRepository eventTicketFieldRepository;
+
+    @Autowired
+    VoucherRepository voucherRepository;
+
+    @Autowired
+    private EventVoucherRepository eventVoucherRepository;
 
     public Mono<List<RetrieveEventResponse>> retrieveEvents(Session session, String page, String size) {
         return Mono
@@ -67,15 +82,76 @@ public class OrganizerEventUseCase {
                         }));
     }
 
-    public Mono<CreateEventRequest> saveOne(CreateEventRequest request) {
-        //check if the organizer is the owner of the event
-        //check if the event is not exist
-        //save the event
-        //return event detail from getEventById
-        return null;
+    public Mono<RetrieveEventResponse> saveOne(CreateEventRequest request, Session session) {
+        return Mono
+                .fromCallable(() -> jwtAuthenticationUseCase.verify(session.getAccessToken()))
+                .map(decodedJwt -> decodedJwt.getClaim("account_id").as(UUID.class))
+                .flatMap(accountId -> accountRepository.findFirstById(accountId))
+                .switchIfEmpty(Mono.error(new AccountNotFoundException()))
+                .flatMap(account -> {
+                    Event newEvent = Event.builder()
+                            .id(UUID.randomUUID())
+                            .accountId(account.getId())
+                            .name(request.getName())
+                            .description(request.getDescription())
+                            .location(request.getLocation())
+                            .category(request.getCategory())
+                            .time(request.getTime())
+                            .bannerImageUrl(null)
+                            .build();
+
+                    Mono<Event> savedEvent = eventRepository.save(newEvent);
+
+                    Mono<EventTicket> newTicket = savedEvent.flatMap(
+                    event -> Mono.just(EventTicket.builder()
+                                        .id(UUID.randomUUID())
+                                        .eventId(event.getId())
+                                        .name(null)
+                                        .description(null)
+                                        .slots(request.getSlots())
+                                        .price(request.getPrice())
+                                        .build()
+                    ));
+
+                    Mono<EventTicket> savedTicket = newTicket.flatMap(eventTicketRepository::save);
+
+                    savedTicket.flatMapMany(ticket -> {
+                        return Flux.just("name", "phone", "email", "dob")
+                                .map(key -> EventTicketField.builder()
+                                        .id(UUID.randomUUID())
+                                        .eventTicketId(ticket.getId())
+                                        .key(key)
+                                        .build())
+                                .flatMap(eventTicketFieldRepository::save);
+                    });
+
+                    Flux<Voucher> savedVouchers = Flux.fromStream(Arrays.stream(request.getVouchers())
+                            .map(voucher -> Voucher.builder()
+                                    .id(UUID.randomUUID())
+                                    .code(UUID.randomUUID().toString())
+                                    .name(voucher.getName())
+                                    .description(voucher.getDescription())
+                                    .variableAmount(voucher.getVariableAmount())
+                                    .startedAt(voucher.getStartedAt())
+                                    .endedAt(voucher.getEndedAt())
+                                    .build()
+                            ).map(voucherRepository::save)
+                    ).flatMap(voucherMono -> voucherMono);
+
+                    savedVouchers.flatMap(savedVoucher -> {
+                        EventVoucher eventVoucher = EventVoucher.builder()
+                                .id(UUID.randomUUID())
+                                .eventId(newEvent.getId())
+                                .voucherId(savedVoucher.getId())
+                                .build();
+                        return eventVoucherRepository.save(eventVoucher);
+                    }).collectList();
+
+                    return null;
+                });
     }
 
-    public Mono<CreateEventRequest> updateOne(RetrieveEventResponse request) {
+    public Mono<RetrieveEventResponse> updateOne(RetrieveEventResponse request) {
         //check if the organizer is the owner of the event
         //check if the event exist
         //update the event
