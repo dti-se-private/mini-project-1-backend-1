125ac14c541e0389c0fee7218b67f577e836468d
nurfad7 <nurfad7@gmail.com>
2024-12-09 16:35:08 +0700

    add create and delete feedback feature

---
+++ src/main/java/org/dti/se/miniproject1backend1/inners/models/valueobjects/profile/RetrieveAllFeedbackResponse.java
@@ -0,0 +1,22 @@
+package org.dti.se.miniproject1backend1.inners.models.valueobjects.profile;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.dti.se.miniproject1backend1.inners.models.Model;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class RetrieveAllFeedbackResponse extends Model {
+    private UUID transactionId;
+    private UUID eventId;
+    private String eventName;
+    private OffsetDateTime time;
+    private RetrieveFeedbackResponse feedback;
+}
