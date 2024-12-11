00851e78495e2e4b47514328fac2548322b42fee
nurfad7 <nurfad7@gmail.com>
2024-12-11 15:52:15 +0700

    add function in TransactionRepository

--- src/main/java/org/dti/se/miniproject1backend1/outers/repositories/ones/TransactionRepository.java
+++ src/main/java/org/dti/se/miniproject1backend1/outers/repositories/ones/TransactionRepository.java
@@ -3,7 +3,9 @@ package org.dti.se.miniproject1backend1.outers.repositories.ones;
 import org.dti.se.miniproject1backend1.inners.models.entities.Transaction;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
@@ -14,4 +16,6 @@ public interface TransactionRepository extends R2dbcRepository<Transaction, Long
     Mono<Void> deleteAllByEventId(UUID eventId);
 
     Mono<Void> deleteById(UUID id);
+
+    Flux<Transaction> findByAccountId(UUID accountId, Pageable pageable);
 }
